a203d5bdb42e99cd5a043ff4aacfab2958a9906d
veenus1128 <sharmilaveenus123@gmail.com>
2024-07-08 10:11:58 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Navbar from './components/navbar';
+import Register from './components/register';
+import {BrowserRouter as Router,Routes,Route}from 'react-router-dom';
+const root=ReactDOM.createRoot(document.getElementById('root'))
+
+//virtual Root( V DOM)
+root.render(
+  
+  <div>
+    <Router>
+      <Navbar/>
+      <Routes>
+        <Route path="/register" element={<Register/>}/>
+        <Route path="/login" element={<h1>Login</h1>}/>
+        <Route path="/logout" element={<h1>Logout</h1>}/>
+
+      </Routes>
+    </Router>
+  
+  </div>
+  
+ )
